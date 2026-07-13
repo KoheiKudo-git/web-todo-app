52815c77e60bc180a2ee79b5a5fa044481616fe4
プログラミング <se24085e@st.omu.ac.jp>
2026-07-14 02:29:58 +0900

    Initial commit

--- app/vite.config.js
+++ app/vite.config.js
@@ -1,24 +1,22 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { bunny } from 'laravel-vite-plugin/fonts';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
-            fonts: [
-                bunny('Instrument Sans', {
-                    weights: [400, 500, 600],
-                }),
+            input: [
+                'resources/sass/app.scss',
+                'resources/js/app.js',
             ],
+            refresh: true,
         }),
-        tailwindcss(),
     ],
+    // ここから追加
     server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
+        host: '0.0.0.0',
+        hmr: {
+            host: 'localhost',
         },
     },
+    // ここまで追加
 });
